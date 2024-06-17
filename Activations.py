5fd661615bfb435905c5df3040a66b6661780092
AdrianSctr <92805142+AdrianSctr@users.noreply.github.com>
2024-06-17 16:03:16 +0200

    Update Activations.py

--- Activations.py
+++ Activations.py
@@ -12,34 +12,15 @@ class Softmax:
         return self.result
 
     def backward(self, grad_output, learning_rate):
-        #probabilities = self.input
-        #size = self.input.shape[0]
-        #return grad_output #* probabilities * (1 - probabilities)
-        # Calcul de la dérivée de la fonction softmax par rapport à ses entrées
-        """"
-        softmax_output = self.result
-        batch_size = softmax_output.shape[0]
-        grad_input = np.empty_like(softmax_output)
-        for i in range(batch_size):
-            jacobian_matrix = -np.outer(softmax_output[i], softmax_output[i]) + np.diag(softmax_output[i])
-            grad_input[i] = np.dot(grad_output[i], jacobian_matrix)
-        """
-        """
-        batch_size = self.result.shape[0]
-        grad_input = (self.result - grad_output) / batch_size
-        """
-        softmax_output = self.result
-        batch_size = softmax_output.shape[0]
-        grad_input = np.empty_like(softmax_output)
         
-        for i in range(batch_size):
-            # Calcul de la matrice jacobienne pour le i-ème exemple
-            s = softmax_output[i].reshape(-1, 1)
-            jacobian_matrix = np.diagflat(s) - np.dot(s, s.T)
-            # Calcul du gradient de l'entrée
+        num_samples = grad_output.shape[0]
+        grad_input = np.empty_like(grad_output)
+
+        for i in range(num_samples):
+            y = self.result[i].reshape(-1, 1)
+            jacobian_matrix = np.diagflat(y) - np.dot(y, y.T)
             grad_input[i] = np.dot(jacobian_matrix, grad_output[i])
         
-        return grad_input
         return grad_input
     
 class Sigmoid:
