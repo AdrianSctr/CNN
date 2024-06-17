9ce339af749c952c5dade5ab0a4d6fa800cc4361
AdrianSctr <92805142+AdrianSctr@users.noreply.github.com>
2024-06-17 16:04:27 +0200

    Update Dropout.py

--- Dropout.py
+++ Dropout.py
@@ -4,10 +4,17 @@ class Dropout:
     def __init__(self, dropout_rate=0.25):
         self.dropout_rate = dropout_rate
         self.mask = None
+        self.training = True
         
     def forward(self, input):
-        self.mask = (np.random.rand(*input.shape) < self.dropout_rate) / self.dropout_rate
-        return input * self.mask
-    
+        if self.training:
+            self.mask = (np.random.rand(*input.shape) < self.dropout_rate) / self.dropout_rate
+            return input * self.mask
+        else:
+            return input
+        
     def backward(self, grad_output, learning_rate):
         return grad_output * self.mask
+    
+    def set_training(self, training=True):
+        self.training = training
